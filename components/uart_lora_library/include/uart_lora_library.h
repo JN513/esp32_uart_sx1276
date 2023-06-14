d55921a59f733d20b64d3695e7d3eaa681af12dd
JN513 <jn5.guape@gmail.com>
2023-06-14 11:38:51 -0300

    criando função de callback e setcalback, arrumando header file, etc

--- components/uart_lora_library/include/uart_lora_library.h
+++ components/uart_lora_library/include/uart_lora_library.h
@@ -3,35 +3,85 @@
 
 #include <stdio.h>
 #include <string.h>
-
-#include "esp_system.h" // Biblioteca de funções do sistema
-#include "esp_event.h"  // Biblioteca de eventos
-#include "esp_log.h"    // Biblioteca de log
+#include "esp_log.h" // Biblioteca de log
 
 #include "freertos/FreeRTOS.h" // Biblioteca de sistema de tarefas
 #include "freertos/task.h"     // Biblioteca de tarefas
 #include "freertos/semphr.h"   // Biblioteca de sistema de mutex
 #include "freertos/queue.h"    // Biblioteca de filas
 
-#include "soc/soc.h" // Biblioteca de configuração do chip
-
-#include "esp_task_wdt.h" // Biblioteca de tarefas do WDT
-
 #include "driver/gpio.h"
-
 #include "driver/uart.h"
 
-#define BUFF_SIZE 1024
-#define PATTERN_CHR_NUM 3
+#include "definitions.h"
 
-const char *TAG = "SX1276_UART";
+// #define BUFF_SIZE 1024
+// #define PATTERN_CHR_NUM 3
+// #define BROADCAST_ADDRESS 0xFF
+
+static const char *LORA_TAG = "SX1276_UART";
 
 static QueueHandle_t uart_queue;
 
-static int sendData(const char *data);
-static void configUart(int txpin, int rxpin);
-static void uart_event_task(void *pvParameters);
-static void callback(uint8_t *data, size_t len);
-static void setCallback();
+typedef struct
+{
+    gpio_num_t rxPin;
+    gpio_num_t txPin;
+    gpio_num_t m0Pin;
+    gpio_num_t m1Pin;
+    gpio_num_t auxPin;
+
+    int baudRate;
+    int frequency;
+    uint8_t mode;
+
+    uart_port_t uart_port;
+} e32_config_t;
+
+int sendData(const char *data);
+void configUart(int txpin, int rxpin);
+void uart_event_task(void *pvParameters);
+void setCallback(void (*callback)(uint8_t *data, size_t len));
+
+void setMode(uint8_t mode);
+void SetAddress(uint16_t val);
+void SetAddressH(uint8_t val);
+void SetAddressL(uint8_t val);
+void SetAirDataRate(uint8_t val);
+void SetUARTBaudRate(uint8_t val);
+void SetSpeed(uint8_t val);
+void SetOptions(uint8_t val);
+void SetChannel(uint8_t val);
+void SetParityBit(uint8_t val);
+
+// functions to set the options
+void SetTransmissionMode(uint8_t val);
+void SetPullupMode(uint8_t val);
+void SetWORTIming(uint8_t val);
+void SetFECMode(uint8_t val);
+void SetTransmitPower(uint8_t val);
+
+uint8_t getMode();
+uint16_t GetAddress();
+
+// methods to get module data
+uint8_t GetModel();
+uint8_t GetVersion();
+uint8_t GetFeatures();
+
+uint8_t GetAddressH();
+uint8_t GetAddressL();
+uint8_t GetAirDataRate();
+uint8_t GetUARTBaudRate();
+uint8_t GetChannel();
+uint8_t GetParityBit();
+uint8_t GetTransmissionMode();
+uint8_t GetPullupMode();
+uint8_t GetWORTIming();
+uint8_t GetFECMode();
+uint8_t GetTransmitPower();
+
+uint8_t GetOptions();
+uint8_t GetSpeed();
 
 #endif
--- components/uart_lora_library/uart_lora_library.c
+++ components/uart_lora_library/uart_lora_library.c
@@ -1,31 +1,37 @@
+
 #include "uart_lora_library.h"
 
-static int sendData(const char *data)
-{
-    const int len = strlen(data);
-    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
-    ESP_LOGI(TAG, "Enviou %d bytes", txBytes);
+void (*callback_function)(uint8_t *data, size_t len) = NULL;
 
-    return txBytes;
-}
-
-static void configUart(int txpin, int rxpin)
+void configUart(int txpin, int rxpin)
 {
     uart_config_t uart_config = {
         .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    uart_driver_install(UART_NUM_1, BUFF_SIZE * 2, BUFF_SIZE * 2, 20, &uart_queue, 0);
 
     uart_param_config(UART_NUM_1, &uart_config);
 
     uart_set_pin(UART_NUM_1, txpin, rxpin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
-    uart_driver_install(UART_NUM_1, BUFF_SIZE * 2, BUFF_SIZE * 2, 20, &uart_queue, 0);
+    xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 12, NULL);
 }
 
-static void uart_event_task(void *pvParameters)
+int sendData(const char *data)
+{
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(LORA_TAG, "Enviou %d bytes", txBytes);
+
+    return txBytes;
+}
+
+void uart_event_task(void *pvParameters)
 {
     uart_event_t event;
     size_t buff_size;
@@ -37,22 +43,19 @@ static void uart_event_task(void *pvParameters)
         {
             bzero(data, BUFF_SIZE); // tranforma em zero todos os elementos do meu vetor de caracteres
 
-            ESP_LOGI(TAG, "uart[%d] event:", 1);
+            ESP_LOGI(LORA_TAG, "uart[%d] event:", 1);
 
             switch (event.type)
             {
             // Event of UART receving data
-            /*We'd better handler data event fast, there would be much more data events than
-            other types of events. If we take too much time on data event, the queue might
-            be full.*/
             case UART_DATA: // Recebe algo
-                ESP_LOGI(TAG, "[UART DATA]: %d", event.size);
+                ESP_LOGI(LORA_TAG, "[UART DATA]: %d", event.size);
                 uart_read_bytes(UART_NUM_1, data, event.size, portMAX_DELAY);
-                callback(data, event.size);
+                callback_function(data, event.size);
                 break;
             // Event of HW FIFO overflow detected
             case UART_FIFO_OVF:
-                ESP_LOGI(TAG, "hw fifo overflow");
+                ESP_LOGI(LORA_TAG, "hw fifo overflow");
                 // If fifo overflow happened, you should consider adding flow control for your application.
                 // The ISR has already reset the rx FIFO,
                 // As an example, we directly flush the rx buffer here in order to read more data.
@@ -61,27 +64,27 @@ static void uart_event_task(void *pvParameters)
                 break;
             // Event of UART ring buffer full
             case UART_BUFFER_FULL:
-                ESP_LOGI(TAG, "ring buffer full");
+                ESP_LOGI(LORA_TAG, "ring buffer full");
                 uart_flush_input(UART_NUM_1);
                 xQueueReset(uart_queue);
                 break;
             // Event of UART RX break detected
             case UART_BREAK:
-                ESP_LOGI(TAG, "uart rx break");
+                ESP_LOGI(LORA_TAG, "uart rx break");
                 break;
             // Event of UART parity check error
             case UART_PARITY_ERR:
-                ESP_LOGI(TAG, "uart parity error");
+                ESP_LOGI(LORA_TAG, "uart parity error");
                 break;
             // Event of UART frame error
             case UART_FRAME_ERR:
-                ESP_LOGI(TAG, "uart frame error");
+                ESP_LOGI(LORA_TAG, "uart frame error");
                 break;
             // UART_PATTERN_DET
             case UART_PATTERN_DET:
                 uart_get_buffered_data_len(UART_NUM_1, &buff_size);
                 int pos = uart_pattern_pop_pos(UART_NUM_1);
-                ESP_LOGI(TAG, "[UART PATTERN DETECTED] pos: %d, buffered size: %d", pos, buff_size);
+                ESP_LOGI(LORA_TAG, "[UART PATTERN DETECTED] pos: %d, buffered size: %d", pos, buff_size);
                 if (pos == -1)
                 {
                     // There used to be a UART_PATTERN_DET event, but the pattern position queue is full so that it can not
@@ -95,13 +98,13 @@ static void uart_event_task(void *pvParameters)
                     uint8_t pat[PATTERN_CHR_NUM + 1];
                     memset(pat, 0, sizeof(pat));
                     uart_read_bytes(UART_NUM_1, pat, PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
-                    ESP_LOGI(TAG, "read data: %s", data);
-                    ESP_LOGI(TAG, "read pat : %s", pat);
+                    ESP_LOGI(LORA_TAG, "read data: %s", data);
+                    ESP_LOGI(LORA_TAG, "read pat : %s", pat);
                 }
                 break;
             // Others
             default:
-                ESP_LOGI(TAG, "uart event type: %d", event.type);
+                ESP_LOGI(LORA_TAG, "uart event type: %d", event.type);
                 break;
             }
         }
@@ -112,7 +115,7 @@ static void uart_event_task(void *pvParameters)
     vTaskDelete(NULL);
 }
 
-static void callback(uint8_t *data, size_t len)
+void setCallback(void (*callback)(uint8_t *data, size_t len))
 {
-    ESP_LOGI(TAG, "[UART STR]: %s", data);
+    callback_function = callback;
 }
