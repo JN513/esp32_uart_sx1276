ae6eb00e55e99ba1fb04d1c0abc9d9f360acf8b7
JN513 <jn5.guape@gmail.com>
2023-06-14 11:39:03 -0300

    compilando teste para o esp32s3

--- main/esp32_uart_sx1276.c
+++ main/esp32_uart_sx1276.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <string.h>
-
-#include "esp_system.h" // Biblioteca de funções do sistema
-#include "esp_event.h"  // Biblioteca de eventos
-#include "esp_log.h"    // Biblioteca de log
+#include "esp_log.h" // Biblioteca de log
 
 #include "freertos/FreeRTOS.h" // Biblioteca de sistema de tarefas
 #include "freertos/task.h"     // Biblioteca de tarefas
 #include "freertos/semphr.h"   // Biblioteca de sistema de mutex
 #include "freertos/queue.h"    // Biblioteca de filas
 
-#include "soc/soc.h" // Biblioteca de configuração do chip
-
-#include "esp_task_wdt.h" // Biblioteca de tarefas do WDT
-
-#include "driver/gpio.h"
-
-#include "driver/uart.h"
-
 #include "uart_lora_library.h"
+
+static const char *TAG = "esp";
+void callback(uint8_t *data, size_t len)
+{
+    ESP_LOGI(TAG, "[UART STR]: %s", data);
+}
 
 void app_main(void)
 {
     configUart(17, 18);
+    setCallback(*callback);
 
-    xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 12, NULL);
+    while (1)
+    {
+        sendData("Ola");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
 }
